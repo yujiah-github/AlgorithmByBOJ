acf76fc936aceee85090256aafaa5b880cb14afa
yujiah-github <cil05265@naver.com>
2022-03-24 11:24:23 +0900

    feat: 나이순 정렬

---
+++ silver5/10814.py
@@ -0,0 +1,13 @@
+import sys
+n = int(sys.stdin.readline())
+member_lst = []
+
+for i in range(n):
+    age, name = map(str,sys.stdin.readline().split().strip())
+    age = int(age)
+    member_lst.append((age, name))
+
+member_lst.sort(key = lambda x : x[0])	## (age, name)에서 age만 비교
+
+for i in member_lst:
+    print(i[0], i[1])
