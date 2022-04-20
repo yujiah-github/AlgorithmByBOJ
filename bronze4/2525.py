32de1039a4f63d4a1f12ebc70f2f99b629db5031
yujiah-github <cil05265@naver.com>
2022-04-20 14:28:43 +0900

    Feat: oven watch

--- bronze4/2525.py
+++ bronze4/2525.py
@@ -1,3 +1,13 @@
-if(n[0] == n[1]):
-        money = 1000 + (n[0] * 100)
-        print(money)
+H, M = map(int, input().split())
+timer = int(input()) 
+
+H += timer // 60
+M += timer % 60
+
+if M >= 60:
+    H += 1
+    M -= 60
+if H >= 24:
+    H -= 24
+
+print(H,M)
