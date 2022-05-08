be040b2882072114effcb670c40611935d49652d
yujiah-github <cil05265@naver.com>
2022-05-09 00:56:45 +0900

    Feat: 쇠막대기

---
+++ silver3/10799.py
@@ -0,0 +1,19 @@
+import sys
+bar_razor = list(sys.stdin.readline().strip())
+answer = 0
+st = []
+
+for i in range(len(bar_razor)):
+    if bar_razor[i] == '(':
+        st.append('(')
+
+    else:
+        if bar_razor[i-1] == '(': 
+            st.pop()
+            answer += len(st)
+
+        else:
+            st.pop() 
+            answer += 1 
+
+print(answer)
