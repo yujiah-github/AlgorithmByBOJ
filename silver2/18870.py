6931896edc9b96f2457423a93d3d8eee8e3c228d
yujiah-github <cil05265@naver.com>
2022-05-17 13:55:02 +0900

    Refactor: 좌표 압축에 주석 추가

--- silver2/18870.py
+++ silver2/18870.py
@@ -1,7 +1,7 @@
 import sys
 n = int(sys.stdin.readline())
 x = list(map(int, sys.stdin.readline().rstrip().split()))
-numSet = set(x)
+numSet = set(x) #set 함수를 사용하고 다시 list()를 해주어야한다
 numSort = list(numSet)
 numSort.sort()
 
