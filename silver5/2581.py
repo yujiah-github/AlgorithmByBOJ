54dbb8c6861a635079ebcbe102f6f2476a025738
yujiah-github <cil05265@naver.com>
2022-06-21 14:17:33 +0900

    Refactor: 소수 코드 수정

--- silver5/2581.py
+++ silver5/2581.py
@@ -1,17 +1,19 @@
-import sys
-M = int(sys.stdin.readline())
-N = int(sys.stdin.readline())
-num_list = []
-while M<N+1:
-    for i in range(2,M):
-        if M%i == 0:
-            break
-    num_list.append(M)
-    i += 1
-M += 1
+start_num = int(input())
+last_num = int(input())
 
-if len(num_list) == 0:
-    print(-1)
+sosu_list = []
+for num in range(start_num, last_num+1):
+    error = 0
+    if num > 1 :
+        for i in range(2, num):  # 2부터 num-1까지
+            if num % i == 0:
+                error += 1
+                break  # 2부터 num-1까지 나눈 몫이 0이면 error가 증가하고 for문을 끝냄
+        if error == 0:
+            sosu_list.append(num)  # error가 없으면 소수리스트에 추가
+            
+if len(sosu_list) > 0 :
+    print(sum(sosu_list))
+    print(min(sosu_list))
 else:
-    print(sum(num_list))
-    print(min(num_list))
+    print(-1)
