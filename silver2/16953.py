2e756611db435083b307ea496b26985dd47fb4d4
yujiah-github <cil05265@naver.com>
2022-08-18 14:54:12 +0900

    Feat: A -> B 추가

---
+++ silver2/16953.py
@@ -0,0 +1,17 @@
+import sys
+
+a,b = map(int,sys.stdin.readiline().split())
+r = 1
+while(b!=a):
+    r+=1
+    temp = b
+    if b%10 == 1:
+        b//=10
+    elif b%2 == 0:
+        b//=2
+    
+    if temp == b:
+        print(-1)
+        break
+else:
+    print(r)
