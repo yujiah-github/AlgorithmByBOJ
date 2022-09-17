d6fae455f43af683193c80741716fc529c4406ae
yujiah-github <cil05265@naver.com>
2022-09-17 17:43:53 +0900

    Feat: 선 긋기 추가

---
+++ Gold5/2170.py
@@ -0,0 +1,29 @@
+from re import X
+import sys
+
+n = int(sys.stdin.readline())
+lines = []
+
+for _ in range(n):
+    lines.append(tuple(map(int, input().split())))
+
+lines.sort()
+
+x = lines[0][0]
+y = lines[0][1]
+ans = 0
+
+for i in range(1, n):
+    if y >= lines[i][1]:
+        continue
+    
+    elif lines[i][0] <= y < lines[i][1]:
+        y = lines[i][1]
+    
+    elif right < lines[i][0]:
+        ans += y - x
+        x = lines[i][0]
+        y = lines[i][1]
+
+ans += y - x
+print(ans)
