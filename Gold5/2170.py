eeead2f8f9859eb19bc20069c48c8585f31f520c
yujiah-github <cil05265@naver.com>
2022-09-17 23:37:32 +0900

    Refactor: 선 긋기 수정

---
+++ Gold5/2170.py
@@ -0,0 +1,28 @@
+import sys
+
+n = int(sys.stdin.readline())
+lines = []
+
+for _ in range(n):
+    lines.append(tuple(map(int, input().split())))
+
+lines.sort()
+
+x = lines[0][0]
+y = lines[0][1]
+ans = 0
+
+for i in range(1, n):
+    if y >= lines[i][1]:
+        continue
+
+    elif lines[i][0] <= y < lines[i][1]:
+        y = lines[i][1]
+
+    elif y < lines[i][0]:
+        ans += y - x
+        x = lines[i][0]
+        y = lines[i][1]
+
+ans += y - x
+print(ans)
