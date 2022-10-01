1921c9c82c6c3f3bd1ec7d97391cadec1f65c413
yujiah-github <cil05265@naver.com>
2022-10-01 14:12:04 +0900

    Refactor: 1 수정

--- silver3/4375.py
+++ silver3/4375.py
@@ -1,18 +1,15 @@
-def solution(n) :
-  count = 1
-  div = 1
-  while True :
-    if count == 1 : left = div%n
-    else : left = ((div%n)*(10%n)+1)%n
-    
-    if left == 0 : return count
-    else :
-      div = left
-      count += 1
-
 while True :
   try :
     n = int(input())
-    print(solution(n))
   except :
     break
+
+  num = 0
+  i = 1
+while True :
+    num = num * 10 + 1
+    num %= n
+    if num == 0 :
+      print(i)
+      break
+    i+=1
