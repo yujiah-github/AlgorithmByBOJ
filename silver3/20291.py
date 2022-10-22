17f1ed98bb3416c8189904ad5816d674e3e693fc
yujiah-github <cil05265@naver.com>
2022-10-23 00:33:26 +0900

    Refactor: 파일 정리 코드 수정

--- silver3/20291.py
+++ silver3/20291.py
@@ -2,17 +2,12 @@ def file_clean(n):
     ans = {}
     for _ in range(n):
 
-        # 입력받은 문자에서 확장자만 저장하자.
-        split_s = input().split(".")[1]
+        split = input().split(".")[1]
+        ans.setdefault(split, 0)
+        ans[split] += 1
 
-        # 확장자가 없으면 0으로 초기화한 후 1 더하고, 있으면 그냥 1 더하자.
-        ans.setdefault(split_s, 0)
-        ans[split_s] += 1
-
-    # 확장자 오름차순 정렬
     for k, v in sorted(ans.items(), key=lambda x: x[0]):
         print(k, v)
-
 
 n = int(input())
 file_clean(n)
