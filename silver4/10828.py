598c581d92b2fa1b47e0a97a6f30b76b70e79821
yujiah-github <cil05265@naver.com>
2022-12-12 18:57:55 +0900

    Feat: 스택 추가(복습)

--- silver4/10828.py
+++ silver4/10828.py
@@ -1,29 +1,29 @@
 import sys
-n = int(sys.stdin.readline()) #명령의 수 입력
-stack = [] #리스트 생성
-for i in range(n):
-    x = sys.stdin.readline().split() #명령어와 숫자 입력
+n = int(sys.stdin.readline()) #명령 숫자 입력 받기
+lst = [] #명령어를 저장할 리스트
+for i in range(n): #입력 받은 숫자만큼 반복
+    commend = sys.stdin.readline().split() # commend에서 명령어와 숫자를 받음
+    if commend[0] == 'push': #명령어가 push인 경우
+        lst.append(commend[1]) #명령어가 push라면 lst[1]의 수를 리스트에 추가
     
-    if x[0] == 'push': #명령어가 푸쉬인 경우
-        stack.append(x[1]) #list_num에 추가 (입력값이 명령어와 숫자로 구성되어 있으므로 인덱스는 0과 1밖에 없다)
+    elif commend[0] == 'pop': #명령어가 pop인 경우
+        if len(lst) == 0:
+            print(-1)
+        else: 
+            x = lst.pop() #리스트에서 가장 위에 있는 숫자 빼기
+            print(x) #가장 위에 있던 숫자 출력
     
-    elif x[0] == 'pop': #명령어가 팝인 경우
-        if len(stack) == 0:
-            print(-1) #정수가 없으면 -1을 출력
-        else:
-            print(stack.pop()) #제거
-            
-    elif x[0] == 'size': #명령어가 size인 경우
-        print(len(stack))
+    elif commend[0] == 'size': #명령어가 size인 경우
+        print(len(lst))  #리스트의 크기 반환
     
-    elif x[0] == 'empty': #empty인 경우
-        if len(stack) == 0:
+    elif commend[0] == 'empty': #명령어가 empty인 경우
+        if len(lst) == 0: #리스트가 비어있으면 1을 반환
             print(1)
         else:
             print(0)
             
-    elif x[0] == 'top':
-        if len(stack) ==0:
+    elif commend[0]== 'top': #명령어가 top인 경우
+        if len(lst) == 0: #리스트가 비어있으면 -1을 반환, 아닐 경우 가장 끝 수를 반환
             print(-1)
         else:
-            print(stack[-1]) 
+            print(lst[-1])
