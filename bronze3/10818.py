6599a2f6bd6f4f7fab1b8f6f01fe3dbc73ab1241
yujiah-github <cil05265@naver.com>
2023-01-22 00:15:46 +0900

    Feat: 최소, 최대 추가

--- bronze3/10818.py
+++ bronze3/10818.py
@@ -1,3 +1,9 @@
 cnt = int(input())
 numbers = list(map(int, input().split()))
 print(min(numbers),max(numbers))
+
+import sys
+N = int(sys.stdin.readline())
+a = list(map(int, sys.stdin.readline().split()))
+a.sort()
+print(a[0], a[-1])
