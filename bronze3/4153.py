1da07dbe1d47b0af7faa808b240596148c410009
yujiah-github <cil05265@naver.com>
2023-02-04 22:31:58 +0900

    Feat: 직각 삼각형 추가

--- bronze3/4153.py
+++ bronze3/4153.py
@@ -1,15 +1,12 @@
-#세 수를 입력 받고, 리스트로 만들어 준다.
-#리스트에서 가장 큰 수를 고른다.
-#피타고라스 정리를 활용하여 식을 만들어주고 직각이 맞으면 right, 틀리면 wrong을 반환한다.
-# 000이 입력 되면, 프로그램을 종료한다.
 import sys
+
 while True:
-    numList = list(map(int, sys.stdin.readline().strip().split()))
+    numList = list(map(int, sys.stdin.readline().split()))
     if sum(numList) == 0:
         break
     maxNum = max(numList)
     numList.remove(maxNum)
-    if numList[0]**2 + numList[1] **2 == maxNum ** 2:
+    if numList[0] ** 2 + numList[1] ** 2 == maxNum ** 2:
         print("right")
     else:
         print("wrong")
