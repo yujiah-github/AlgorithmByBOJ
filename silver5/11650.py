77ef9dd5d75829f29cd90fac0d180bd67cd350bf
yujiah-github <cil05265@naver.com>
2023-02-07 23:25:15 +0900

    Feat: 좌표 정렬하기 추가

--- silver5/11650.py
+++ silver5/11650.py
@@ -1,10 +1,12 @@
 import sys
+
 N = int(sys.stdin.readline())
-list  = []
+lst = []
+
 for i in range(N):
-    x,y = map(int,sys.stdin.readline().split())
-    list.append((x,y))
+    x,y = map(int, sys.stdin.readline().split())
+    lst.append((x,y))
     
-list.sort(key = lambda x: (x[0],x[1]))
-for i in list:
-    print(i[0],i[1])
+lst.sort(key = lambda x: (x[0], x[1]))
+for i in lst:
+    print(i[0], i[1])
